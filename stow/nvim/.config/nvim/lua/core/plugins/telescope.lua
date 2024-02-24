9dcc36a59975a38a36692336dcbeacf8115c617d
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-02-24 10:37:56 -0800

    Telescope file search applied to current directory only, like any IDE

--- stow/nvim/.config/nvim/lua/core/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/core/plugins/telescope.lua
@@ -11,11 +11,21 @@ require('telescope').setup {
   },
 }
 
+
 local M = {}
+
+function M.GetOpenedDirectory()
+  local args = vim.fn.argv()
+  if #args > 0 and vim.fn.isdirectory(args[1]) == 1 then
+    return args[1]
+  end
+  return nil
+end
 
 M.find_files = function()
   require('telescope.builtin').find_files {
-    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+    -- Get the list of arguments passed to Neovim
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', M.GetOpenedDirectory()},
     previewer = false
   }
 end
@@ -48,7 +58,7 @@ vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc
 vim.keymap.set('n', '<leader>sf', M.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sc', M.find_config_files, { desc = '[S]earch [C]onfig files for docs' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord under cursor' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
