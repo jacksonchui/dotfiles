f4d351d988b4bff11a7afa2e37f65c1291067309
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-03 13:29:11 -0800

    simplifying tmux status bar, increasing blur on window to make it easier to focus on

--- stow/wezterm/.wezterm.lua
+++ stow/wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 -- [config options](https://wezfurlong.org/wezterm/config/lua/config/index.html)
 config.color_scheme = 'Material Palenight (base16)'--'AdventureTime'
 config.window_background_opacity = 0.5
-config.macos_window_background_blur = 42
+config.macos_window_background_blur = 84
 config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font_size = 14.0
 config.line_height = 1.2
