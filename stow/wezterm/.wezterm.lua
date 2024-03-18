721f14790f2077ae866290e43b7c1103d297baf9
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2024-03-17 19:24:02 -0700

    Revert to old tmux prefix, fix ctrl-a,e, opacity is less distracting, show prefix highlighting again...I don't need a complicated powerline

--- stow/wezterm/.wezterm.lua
+++ stow/wezterm/.wezterm.lua
@@ -3,12 +3,15 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- [config options](https://wezfurlong.org/wezterm/config/lua/config/index.html)
-config.color_scheme = 'Material Palenight (base16)'--'AdventureTime'
-config.window_background_opacity = 0.5
+config.color_scheme = 'Material Palenight (base16)'
+config.window_background_opacity = 0.69
 config.macos_window_background_blur = 84
 config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font_size = 14.0
 config.line_height = 1.2
+
+-- hopefully this fixes key presses
+config.use_ime = false
 
 -- disables titlebar and borders
 config.window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW"
