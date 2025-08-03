ceb6c764cd9080a7f273c27bb6c96616ca9f28e9
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2025-08-03 00:22:56 -0700

    Migrate `Mason-lspconfig` to v2.0.0
    See https://github.com/mason-org/mason-lspconfig.nvim/releases/tag/v2.0.0

--- stow/nvim/.config/nvim/lua/core/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/core/plugins/lsp.lua
@@ -52,50 +52,36 @@ local home = os.getenv("HOME") or os.getenv("USERPROFILE")
 --
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
-local server_opts_mp = {
-    clangd = {
-        cmd = { "clangd",
-                "--all-scopes-completion",      -- Code completion from all visible scopes
-                "--background-index",
-                -- --compile_args_from=filesystem   -- read from filesystem instead of compile_commands.json
-                "--clang-tidy",                 -- integrate checks into editor
-                "--completion-parse=always",   -- always run cc, at the cost of compute
-                "--completion-style=detailed",
-                "--cross-file-rename",          -- lsp will apply across files, faster with --bg-index
-                "--pch-storage=disk",
-                "--log=error",
-                "--enable-config",
-                "--header-insertion=iwyu",      -- Add includes for what is used
-                "-j=4",                         -- num of workers
-        },
-        filetypes = { "c", "cpp", "objc" },
+
+-- migrate to v2.0.0 code away from recursive table
+vim.lsp.config('lua_ls', {
+    Lua = {
+        diagnostics = { globals = { "vim " }, },
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+        completion = { callSnippet = "Replace" },
     },
-    pyright = {},
-    lua_ls = {
-        Lua = {
-            diagnostics = { globals = { "vim " }, },
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            completion = { callSnippet = "Replace" },
-        },
+})
+
+vim.lsp.config('clangd', {
+    cmd = { "clangd",
+            "--all-scopes-completion",      -- Code completion from all visible scopes
+            "--background-index",
+            -- --compile_args_from=filesystem   -- read from filesystem instead of compile_commands.json
+            "--clang-tidy",                 -- integrate checks into editor
+            "--completion-parse=always",   -- always run cc, at the cost of compute
+            "--completion-style=detailed",
+            "--cross-file-rename",          -- lsp will apply across files, faster with --bg-index
+            "--pch-storage=disk",
+            "--log=error",
+            "--enable-config",
+            "--header-insertion=iwyu",      -- Add includes for what is used
+            "-j=4",                         -- num of workers
     },
-    -- https://github.com/williamboman/nvim-lsp-installer/discussions/781
-    arduino_language_server = {
-        cmd = {
-            "arduino-language-server",
-            "-cli-config",
-            home .. "/Library/Arduino15/arduino-cli.yaml",
-            "-cli",
-            "/opt/homebrew/bin/arduino-cli",
-            "-clangd",
-            "/usr/bin/clangd",
-            "-fqbn",
-            "esp32:esp32:esp32",
-        }
-    },
-    rust_analyzer = {},
-    zls = {},
-}
+    filetypes = { "c", "cpp", "objc" },
+})
+
+-- NOTE: Using defaults for the rest of the servers.
 
 -- sourcekit comes bundled with the swift toolchain, so install separately
 local lspconfig = require('lspconfig')
@@ -113,28 +99,14 @@ lspconfig.sourcekit.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- Ensure the servers above are installed
-local mason_lspconfig = require('mason-lspconfig')
-
-mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(server_opts_mp),
+-- NOTE: Go to `:Mason` to further configure installed servers
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = {
+        'fish_lsp',
+        'clangd',
+        'lua_ls',
+        'pyright', 
+        'zls', 
+    }
 }
-
--- Take local `servers` and handle each one to setup
-mason_lspconfig.setup_handlers {
-    function(server)
-        local opts = {
-            capabilities = capabilities,
-            on_attach = on_attach,
-        }
-
-        if not server_opts_mp[server] then
-            print("Custom Opts for ".. server .." are not initialized properly.")
-            return -- stop further execution to avoid errors
-        end
-
-        opts = vim.tbl_deep_extend("keep", server_opts_mp[server], opts)
-        require('lspconfig')[server].setup(opts)
-    end,
-}
-
