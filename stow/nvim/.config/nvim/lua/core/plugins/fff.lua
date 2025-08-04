b1c4139d455d600561e46d7723d86e5ea99bf6d0
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2025-08-03 18:10:50 -0700

    For `fff`, `setup()` through config, because there is no guarantee that it is lazy compatible and does what we want, so we leverage the `config` callback to `setup`
    That makes sense, lazy is just there to gather all the structs of plugins but there is no way for it to tell that it needs to get any work done.

--- stow/nvim/.config/nvim/lua/core/plugins/fff.lua
+++ stow/nvim/.config/nvim/lua/core/plugins/fff.lua
@@ -15,47 +15,54 @@ return {
         desc = "FFFind FFFiles",
       },
     },
-    opts = {
-      width = 0.8,
-      height = 0.8,
-      prompt = '🪿 ',
-      preview = {
-        enabled = true,
-        width = 0.5,
-        max_lines = 100,
-        max_size = 1024 * 1024,
-      },
-      title = 'FFF Files',
-      max_results = 60,
-      max_threads = 4,
-      keymaps = {
-        close = '<C-c>',
-        select = '<CR>',
-        select_split = '<C-s>',
-        select_vsplit = '<C-v>',
-        select_tab = '<C-t>',
-        move_up = { '<Up>', '<C-p>' },
-        move_down = { '<Down>', '<C-n>' },
-        preview_scroll_up = '<C-u>',
-        preview_scroll_down = '<C-d>',
-      },
-      hl = {
-        border = 'FloatBorder',
-        normal = 'Normal',
-        cursor = 'CursorLine',
-        matched = 'IncSearch',
-        title = 'Title',
-        prompt = 'Question',
-        active_file = 'Visual',
-        frecency = 'Number',
-        debug = 'Comment',
-      },
-      debug = {
-        show_scores = false,
-      },
-    },
     config = function()
-      require('fff').setup() -- turns out, I still need to call setup on top of lazy
+      require('fff').setup({
+      -- UI dimensions and appearance
+          width = 0.8,          -- Window width as fraction of screen
+          height = 0.8,         -- Window height as fraction of screen
+          prompt = '🪿 ',       -- Input prompt symbol
+          preview = {
+              enabled = true,
+              width = 0.5,
+              max_lines = 100,
+              max_size = 1024 * 1024, -- 1MB
+          },
+          title = 'FFF Files',  -- Window title
+          max_results = 60,     -- Maximum search results to display
+          max_threads = 4,      -- Maximum threads for fuzzy search
+
+          keymaps = {
+            close = '<C-c>',
+            select = '<CR>',
+            select_split = '<C-s>',
+            select_vsplit = '<C-v>',
+            select_tab = '<C-t>',
+            -- Multiple bindings supported
+            move_up = { '<Up>', '<C-p>' },
+            move_down = { '<Down>', '<C-n>' },
+            preview_scroll_up = '<C-u>',
+            preview_scroll_down = '<C-d>',
+          },
+
+          -- Highlight groups
+          hl = {
+            border = 'FloatBorder',
+            normal = 'Normal',
+            cursor = 'CursorLine',
+            matched = 'IncSearch',
+            title = 'Title',
+            prompt = 'Question',
+            active_file = 'Visual',
+            frecency = 'Number',
+            debug = 'Comment',
+          },
+
+          -- Debug options
+          debug = {
+            show_scores = false,  -- Toggle with F2 or :FFFDebug
+          },
+      })
+
       require('nvim-web-devicons').setup()
     end,
   },
